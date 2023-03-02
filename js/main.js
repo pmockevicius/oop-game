7245b4e1b801d453064137120fc625c92603624c
Paulius Mockevicius <pmockevicius@gmail.com>
2023-03-02 11:58:44 +0100

    functionality to move and display player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log('hello world')
+class Player {
+    constructor(){
+        this.positionX = 0;
+        this.positionY = 0;
+        this.playerElm = document.getElementById("player");
+    }
+    moveLeft(){
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+    moveRight(){
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + "vw";
+    }
+}
+
+
+
+const myPlayer = new Player();
+
+//attach event listeners
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        myPlayer.moveLeft();
+    } else if (e.key === "ArrowRight") {
+        myPlayer.moveRight();
+    }
+});
+
