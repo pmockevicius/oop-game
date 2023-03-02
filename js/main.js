6e0c0437ec090e67eef6766a06b2ca5aeb607401
Paulius Mockevicius <pmockevicius@gmail.com>
2023-03-02 15:58:42 +0100

    create player and obstacles that move down

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 class Player {
     constructor(){
         this.positionX = 0;
@@ -16,7 +18,52 @@ class Player {
 
 
 
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 100;
+        this.obstacleElm = null; //will store a dom element
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // step1: create the element
+        this.obstacleElm = document.createElement('div');
+
+        // step2: add content (ex. innerText) and/or modify attributes 
+        this.obstacleElm.className = "obstacle";
+        this.obstacleElm.style.left = this.positionX + "vw";
+
+        //step3: append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.obstacleElm);
+    }
+    moveDown(){
+        this.positionY--;
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+    }
+}
+
+
+
 const myPlayer = new Player();
+const obstaclesArr = []; //will store instances of the class Obstacle
+
+
+//create new obstacles
+setInterval(function(){
+    const myObstacle = new Obstacle();
+    obstaclesArr.push(myObstacle);
+}, 2000);
+
+
+//move all obstacles
+setInterval(function(){
+    obstaclesArr.forEach( (obstacleInstance) => {
+        obstacleInstance.moveDown();
+    });
+}, 16);
+
 
 //attach event listeners
 document.addEventListener("keydown", (e) => {
